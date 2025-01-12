129ecd6cfe0f1c6d328deaaf21b51cb93374b708
arunikax <srarfian@gmail.com>
2025-01-12 21:25:40 +0700

    home view done

--- 4_wikiGame/tailwind.config.js
+++ 4_wikiGame/tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./views/**/*.hbs"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#0A21C0", // Primary Blue (Button utama)
+          700: "#1D4ED8", // Warna biru tua
+          800: "#1E40AF",
+          300: "#93C5FD",
+          900: "#1E3A8A",
+        },
+        secondary: {
+          darkBlue: "#050A44", // Navy Blue (Navbar dan Button sekunder)
+          darkGray: "#2C2E3A", // Dark Gray (Background)
+        },
+        neutral: {
+          lightGray: "#B3B4BD", // Light Gray (Card dan teks elemen)
+        },
+      },
+    },
   },
   plugins: [],
 };
